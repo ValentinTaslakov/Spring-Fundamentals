53e1048335701c53df5839f7b03afb2569596629
ValentinTaslakov <79251472+ValentinTaslakov@users.noreply.github.com>
2022-05-27 10:16:52 +0300

    Commit mobillele with Gradle

---
+++ My - Mobilelele/myMobileleGradle/src/main/java/bg/softuni/myMobilele/MyMobileleApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.myMobilele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMobileleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMobileleApplication.class, args);
+	}
+
+}
---
+++ My - Mobilelele/myMobileleGradle/src/test/java/bg/softuni/myMobilele/MyMobileleApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.myMobilele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMobileleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
