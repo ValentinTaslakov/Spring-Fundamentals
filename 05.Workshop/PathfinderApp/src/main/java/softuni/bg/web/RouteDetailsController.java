b3e836797bd6880443f28f1d01ca5864ddcb5afb
ValentinTaslakov <79251472+ValentinTaslakov@users.noreply.github.com>
2022-06-15 16:15:34 +0300

    route details

--- 05.Workshop/PathfinderApp/src/main/java/softuni/bg/web/RouteDetailsController.java
+++ 05.Workshop/PathfinderApp/src/main/java/softuni/bg/web/RouteDetailsController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import softuni.bg.model.entities.Route;
 import softuni.bg.services.RouteService;
 
@@ -20,10 +22,12 @@ public class RouteDetailsController {
         this.routeService = routeService;
     }
 
-    @GetMapping("/routes/details/:id")
-    public String routeDetails (Model model){
+    @GetMapping("/routes/details/:id?id=")
+    @ResponseBody
+    public String getRouteDetailsById (@RequestParam String id,
+                                       Model model){
 
-        Optional<Route> optionalRoute = routeService.getRouteById((long)1);
+        Optional<Route> optionalRoute = routeService.getRouteById(Long.valueOf(id));
         Route route = optionalRoute.get();
 
         model.addAttribute("description",route);
