6b2698749a687ebedc67fcdfbecc2063c0457f09
ValentinTaslakov <79251472+ValentinTaslakov@users.noreply.github.com>
2022-06-24 13:51:12 +0300

    Exam Preparation lector sollution

---
+++ 10. Exam Preparation/Battle Ships - exam preparation/src/main/java/com/example/demo/models/dto/ShipDTO.java
@@ -0,0 +1,35 @@
+package com.example.demo.models.dto;
+
+import com.example.demo.models.entities.ShipEntity;
+import com.softuni.battleships.models.Ship;
+
+public class ShipDTO {
+    private long id;
+    private String name;
+    private long health;
+    private long power;
+
+    public ShipDTO(ShipEntity ship) {
+        this.id = ship.getId();
+        this.name = ship.getName();
+        this.power = ship.getPower();
+        this.health = ship.getHealth();
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getHealth() {
+        return health;
+    }
+
+    public long getPower() {
+        return power;
+    }
+}
--- 10. Exam Preparation/Battle Ships - exam preparation/src/main/java/com/example/demo/web/HomeController.java
+++ 10. Exam Preparation/Battle Ships - exam preparation/src/main/java/com/example/demo/web/HomeController.java
@@ -1,19 +1,61 @@
 package com.example.demo.web;
 
+
+import com.example.demo.models.dto.ShipDTO;
+import com.example.demo.models.dto.StartBattleDTO;
+import com.example.demo.services.ShipService;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final ShipService shipService;
+
+    private final UserService userService;
+
+    @ModelAttribute("startBattleDTO")
+    public StartBattleDTO initBattleForm() {
+        return new StartBattleDTO();
+    }
+
+    @Autowired
+    public HomeController(ShipService shipService, UserService userService) {
+        this.shipService = shipService;
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String loggedOutIndex() {
+        if (this.userService.isLoggedIn()) {
+            return "redirect:/home";
+        }
+
         return "index";
     }
 
     @GetMapping("/home")
-    public String loggedInIndex() {
+    public String loggedInIndex(Model model) {
+        if (!this.userService.isLoggedIn()) {
+            return "redirect:/";
+        }
+
+        long loggedUserId = this.userService.getLoggedUserId();
+
+//        List<ShipDTO> ownShips = this.shipService.getShipsOwnedBy(loggedUserId);
+//        List<ShipDTO> enemyShips = this.shipService.getShipsNotOwnedBy(loggedUserId);
+//        List<ShipDTO> sortedShips = this.shipService.getAllSorted();
+
+        model.addAttribute("ownShips", ownShips);
+        model.addAttribute("enemyShips", enemyShips);
+        model.addAttribute("sortedShips", sortedShips);
+
         return "home";
     }
-
 }
